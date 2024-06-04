2a100179e30b04f5515d1068b3411c1d2f56e87b
twotop6357 <twotop6357@gmail.com>
2024-06-04 20:13:13 +0900

    Add ObjectPool_Special
    특수 장애물을 추가하였습니다.

--- Assets/01_Scripts/SH_Temp_Scripts/Impediment/ImpedimentsSpawn.cs
+++ Assets/01_Scripts/SH_Temp_Scripts/Impediment/ImpedimentsSpawn.cs
@@ -11,9 +11,17 @@ public class ImpedimentsSpawn : MonoBehaviour
         new Vector3(-25, 1, 0), 
         new Vector3(-25, 1, -3) 
     };
+
+    private Vector3[] specialSpawnPoints =
+    {
+        new Vector3(-25, 1, 0),
+        new Vector3(-25, 1, -4)
+    };
+
     private int maxObjectCount = 2;
     private float impedimentRespawnTime;
     private float curTime;
+    private int isSpecialPercent;
 
     private void Awake()
     {
@@ -26,7 +34,15 @@ public class ImpedimentsSpawn : MonoBehaviour
         curTime += Time.deltaTime;
         if(curTime > impedimentRespawnTime)
         {
-            SpawnImpediment();
+            isSpecialPercent = Random.Range(0, 10);
+            if(isSpecialPercent >= 0 && isSpecialPercent < 4)
+            {
+                SpawnSpecialImpediment();
+            }
+            else
+            {
+                SpawnImpediment();
+            }
             curTime = 0;
         }
     }
@@ -36,12 +52,19 @@ public class ImpedimentsSpawn : MonoBehaviour
         int curObjectCount = Random.Range(1, maxObjectCount+1);
         GameObject impediment1 = pool.SpawnFromPool("Impediment");
         int impediment1_spawnPointIndex = Random.Range(0, spawnPoints.Length); 
-        impediment1.transform.position = spawnPoints[Random.Range(0, spawnPoints.Length)];
+        impediment1.transform.position = spawnPoints[impediment1_spawnPointIndex];
         if(curObjectCount > 1)
         {
             GameObject impediment2 = pool.SpawnFromPool("Impediment");
             impediment2.transform.position = spawnPoints[GetRandomExcluding(0, spawnPoints.Length, impediment1_spawnPointIndex)];
         }
+    }
+
+    private void SpawnSpecialImpediment()
+    {
+        GameObject specialImpediment = pool.SpawnFromPool("SpecialImpediment");
+        int specialImpediment_spawnPointIndex = Random.Range(0, specialSpawnPoints.Length);
+        specialImpediment.transform.position = specialSpawnPoints[specialImpediment_spawnPointIndex];
     }
 
     private int GetRandomExcluding(int min, int max, int exclude)
