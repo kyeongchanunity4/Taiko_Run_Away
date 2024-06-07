22baed6bc7105a86f8f2dcfb3c91eb93570fb8af
MoonHyang-gi <ansgidrl3@gmail.com>
2024-06-07 20:16:03 +0900

    Add Animation Script
    1. 애니메이션 수정
    2. 랙돌 수정
    3. 애니메이션 스크립트 적용
       - PlayerAnimation 스크립트에서 PlayerController 스크립트에서 작성된 함수에 맞게 애니메이션 배정

---
+++ Assets/01_Scripts/HG_Temp_Scripts/Player/PlayerAnimation.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private PlayerController controller;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        controller = GetComponent<PlayerController>();
+    }
+
+    private void OnEnable()
+    {
+        //controller.OnJumpInput += JumpAnimation;
+        //controller.OnSlideInput += SlideAnimation;
+    }
+
+    private void OnDisable()
+    {
+        //controller.OnJumpInput -= JumpAnimation;
+        //controller.OnSlideInput -= SlideAnimation;
+    }
+
+    private void JumpAnimation()
+    {
+        animator.SetTrigger("Jump");
+    }
+
+    private void SlideAnimation()
+    {
+        animator.SetTrigger("Slide");
+    }
+}
